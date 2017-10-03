9d04e1da57b0771b2648a12d7f003b6caf7f9bf3
FlorenciaFiamingo <florenciafiamingo@hotmail.com>
2017-10-02 22:02:22 -0300

    TP2

---
+++ TP-02/Entidades/Snacks.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades_2017
+{
+    public class Snacks : Producto
+    {
+        private short _cantidadCalorias;
+
+        public Snacks(EMarca marca, string codBarras, ConsoleColor color): base(marca, codBarras, color)
+        {
+            this._cantidadCalorias = 104;
+        }
+        /// <summary>
+        /// Los snacks tienen 104 calorías
+        /// </summary>
+        protected override short CantidadCalorias
+        {
+            get
+            {
+                return this._cantidadCalorias;
+            }
+
+            set 
+            {
+                this._cantidadCalorias = value;
+            }
+        }
+
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("SNACKS");
+            sb.AppendLine((string)this);
+            sb.AppendLine("CALORIAS : "+ this.CantidadCalorias.ToString());
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
