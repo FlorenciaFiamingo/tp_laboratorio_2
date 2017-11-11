944db33059934dd9a6122d5d2763010f7e0edebf
FlorenciaFiamingo <florenciafiamingo@hotmail.com>
2017-11-11 17:14:07 -0300

    TP04

---
+++ TP04/Navegador TP-4 - AlumnoV2/Hilo/Descargador.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net; // Avisar del espacio de nombre
+using System.ComponentModel;
+
+namespace Hilo
+{
+    public class Descargador
+    {
+        private string html;
+        private Uri direccion;
+
+        public delegate void EnProgreso(int progreso);
+        public event EnProgreso enProgreso;
+        public delegate void Completo(string datos);
+        public event Completo completo;
+
+        public Descargador(Uri direccion)
+        {
+            this.html = "";
+            this.direccion = direccion;
+        }
+
+        public void IniciarDescarga()
+        {
+            try
+            {
+                WebClient cliente = new WebClient();
+                cliente.DownloadProgressChanged += new DownloadProgressChangedEventHandler(WebClientDownloadProgressChanged);
+                cliente.DownloadStringCompleted += new DownloadStringCompletedEventHandler(WebClientDownloadCompleted);
+
+                cliente.DownloadStringAsync(direccion, html);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        private void WebClientDownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            this.enProgreso(e.ProgressPercentage);
+        }
+        private void WebClientDownloadCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+            this.html = e.Result;
+            this.completo(this.html);
+        }
+    }
+}
